2ee8ec12359e97af1cf6c04fb0000c6a37bd2f05
Mxnts <maciekawsq@gmail.com>
2024-09-30 05:49:42 +0200

    First commit

---
+++ src/components/footer.js
@@ -0,0 +1,26 @@
+import { pageLinks } from "../data";
+
+export const Footer = () => {
+  return (
+    <footer className="section footer">
+        <ul className="footer-links">
+          {pageLinks.map((link) => {
+            const {id, href, text} = link;
+
+            return (
+              <li key={id}>
+                <a href={href} className="footer-link">{text}</a>
+              </li>
+            );
+          })
+          };
+        </ul>
+        <p className="copyright">
+          copyright &copy; Backroads travel tours company
+          <span id={"date"}>{new Date().getFullYear() }</span> all rights reserved
+        </p>
+      </footer> 
+  )
+}
+
+export default Footer;
